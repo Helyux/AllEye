46170332db8ccefd9799e844be179f2e02ddc041
Helyux <lukas.mahler@elisium.de>
2016-09-11 03:44:00 +0200

    Added some Color

--- checker.py
+++ checker.py
@@ -1,5 +1,8 @@
 # checker.py
 
+from colorama import init
+from termcolor import colored
+init()
 import alarm
 import os
 import sys
@@ -10,6 +13,31 @@ import subprocess
 def currT():
 	t = time.strftime("%H:%M:%S")
 	return t
+
+def getRAM():
+	ram_avail = 0
+	ram_free = 0
+	ram_info_avail = subprocess.check_output('WMIC memorychip get capacity', shell=True).decode("utf-8").split()
+	ram_info_free  = subprocess.check_output('WMIC OS get FreePhysicalMemory /Value', shell=True).decode("utf-8").split("=")
+	del ram_info_avail[0]
+	del ram_info_free[0]
+	ram_info_free = str(ram_info_free[0])
+	ram_info_free = ram_info_free.replace("\n", "")
+	ram_info_free = int(ram_info_free.replace("\r", ""))
+	riegel = len(ram_info_avail)
+	for r in ram_info_avail:
+		ram_avail = ram_avail + int(r)
+	ram_avail = round(ram_avail / 1000000000,2)
+	ram_avail = "{:5.2f}".format(ram_avail)
+	ram_free = round(ram_info_free / 1000000,2)
+	ram_free = "{:5.2f}".format(ram_free)
+	global ram_used
+	ram_used = round(float(ram_avail) - float(ram_free),2)
+	ram_used = "{:5.2f}".format(ram_used)
+	ram_per_r = round(float(ram_avail) / riegel,2)
+	ram_per_r = "{:5.2f}".format(ram_per_r)
+	
+	return riegel,ram_avail,ram_per_r,ram_free,ram_used
 	
 def getProc():
 	##### Prozesse auslesen aus WMIC
@@ -36,54 +64,17 @@ def getProc():
 	
 	##### Zähle Prozesse, berechne durchschnitt
 	proc_num = len(proc_clean)
-	proc_avrg = round(ram_used / proc_num,2)
+	proc_avrg = int(round((float(ram_used) / proc_num)*1000,0))
 	
 	##### Sortiere nach Verbrauch
 	rsc_1 = sorted(proc_clean,key=lambda x: x[1], reverse=True)[0]
-	rsc_1 = "{0: <14} verbraucht: ~ [{1:4.0f}] MB".format(rsc_1[0],rsc_1[1])
+	rsc_1 = "{0: <16} verbraucht: ~ [{1:4.0f}] MB".format(rsc_1[0],rsc_1[1])
 	rsc_2 = sorted(proc_clean,key=lambda x: x[1], reverse=True)[1]
-	rsc_2 = "{0: <14} verbraucht: ~ [{1:4.0f}] MB".format(rsc_2[0],rsc_2[1])
+	rsc_2 = "{0: <16} verbraucht: ~ [{1:4.0f}] MB".format(rsc_2[0],rsc_2[1])
 	rsc_3 = sorted(proc_clean,key=lambda x: x[1], reverse=True)[2]
-	rsc_3 = "{0: <14} verbraucht: ~ [{1:4.0f}] MB".format(rsc_3[0],rsc_3[1])
+	rsc_3 = "{0: <16} verbraucht: ~ [{1:4.0f}] MB".format(rsc_3[0],rsc_3[1])
 	
-	##### Formatiere Output
-	print("")
-	print("Es laufen momentan ["+str(proc_num)+"] Prozesse.")
-	print("===========================================================")
-	print("D.h. ein Prozess verbraucht durchschnittlich ["+str(proc_avrg)+"] GB.")
-	print("-----------------------------------------------------------")
-	print("Die drei Ressource intensivsten Prozesse sind:")
-	print("1. ->",rsc_1)
-	print("2. ->",rsc_2)
-	print("3. ->",rsc_3)
-	print("===========================================================")
-
-def getRAM():
-	ram_avail = 0
-	ram_free = 0
-	ram_info_avail = subprocess.check_output('WMIC memorychip get capacity', shell=True).decode("utf-8").split()
-	ram_info_free  = subprocess.check_output('WMIC OS get FreePhysicalMemory /Value', shell=True).decode("utf-8").split("=")
-	del ram_info_avail[0]
-	del ram_info_free[0]
-	ram_info_free = str(ram_info_free[0])
-	ram_info_free = ram_info_free.replace("\n", "")
-	ram_info_free = int(ram_info_free.replace("\r", ""))
-	riegel = len(ram_info_avail)
-	for r in ram_info_avail:
-		ram_avail = ram_avail + int(r)
-	ram_avail = round(ram_avail / 1000000000,2)
-	ram_free = round(ram_info_free / 1000000,2)
-	global ram_used
-	ram_used = round(ram_avail - ram_free,2)
-	ram_per_r = round(ram_avail / riegel,2)
-	print("Im Computer sind ["+str(riegel)+"] Ram-Riegel eingebaut!")
-	print("===========================================================")
-	print("Gesamtkapazität   : ["+str(ram_avail)+"] GB")
-	print("Ram pro Riegel    : ["+str(ram_per_r)+"] GB")
-	print("-----------------------------------------------------------")
-	print("Kapazität frei    : ["+str(ram_free)+"] GB")
-	print("Kapazität benutzt : ["+str(ram_used)+"] GB")
-	print("===========================================================")
+	return proc_num,proc_avrg,rsc_1,rsc_2,rsc_3
 	
 def getSpace():
 	
@@ -135,27 +126,84 @@ def getSpace():
 		tfix.append(item)
 		
 	del temp
-	
-	##### Formatiere Output
-	print("")
-	print("Alle Internen Festplatten")
-	print("===========================================================")
-	for item in tfix:
-		print("| Driveletter: [{0: <2}] | Size: [{1:5.0f}] GB | Free: [{2:5.0f}] GB |".format(item[0][1],item[2][1],item[1][1]))
-	print("===========================================================")
-	
+	return tfix
 
-while True:
+#def triggerWarning(warn):
+	
+	
+def visualize(ram,proc,space):
+
+	red = 'red'
+	green = 'green'
+	yellow = 'yellow'
+	cyan = 'cyan'
+	
+	#####################################################################################
 	os.system("CLS")
 	date = time.strftime("%d.%m.%Y")
 	print("------------------")
-	print("Date:",date,"|")
-	print("Time:",currT(),"  |")
+	print("Date:",colored(date,cyan),"|")
+	print("Time:",colored(currT(),cyan),"  |")
 	print("------------------")
 	print("")
-	getRAM()
-	getProc()
-	getSpace()
-	time.sleep(10)
+	#####################################################################################
+	if ram[0] >= 2:
+		print("Im Computer sind ["+colored(str(ram[0]),green)+"] Ram-Riegel eingebaut!")
+	else:
+		print("Im Computer ist ["+colored(str(ram[0]),red)+"] Ram-Riegel eingebaut!"+colored("                                           WARNING: THIS MAY SLOW DOWN YOUR PC",red))
+	print("=======================================================================================================================")
+	if float(ram[1]) >= 8:
+		print("Gesamtkapazität   : ["+colored(str(ram[1]),green)+"] GB")
+	else:
+		print("Gesamtkapazität   : ["+colored(str(ram[1]),red)+"] GB")
+	if float(ram[2]) >= 4:
+		print("Ram pro Riegel    : ["+colored(str(ram[2]),green)+"] GB")
+	else:
+		print("Ram pro Riegel    : ["+colored(str(ram[2]),red)+"] GB")
+	print("------------------------------")
+	if float(ram[3]) >= float(ram[1])*0.5:
+		print("Kapazität frei    : ["+colored(str(ram[3]),green)+"] GB")
+		print("Kapazität benutzt : ["+colored(str(ram[4]),green)+"] GB")
+	elif float(ram[3]) >= float(ram[1])*0.3:
+		print("Kapazität frei    : ["+colored(str(ram[3]),yellow)+"] GB"+colored("                                                      WARNING: QUOTA BELOW 50 PERCENT",yellow))
+		print("Kapazität benutzt : ["+colored(str(ram[4]),yellow)+"] GB")
+	elif float(ram[3]) < float(ram[1])*0.3:
+		print("Kapazität frei    : ["+colored(str(ram[3]),red)+"] GB"+colored("                                                      WARNING: QUOTA BELOW 30%",red))
+		print("Kapazität benutzt : ["+colored(str(ram[4]),red)+"] GB")
+	print("=======================================================================================================================")
+	#####################################################################################
+	print("")
+	if proc[0] <= 100:
+		print("Es laufen momentan ["+colored(str(proc[0]),green)+"] Prozesse.")
+	elif proc[0] <= 150:
+		print("Es laufen momentan ["+colored(str(proc[0]),yellow)+"] Prozesse."+colored("                                                  WARNING: THRESHOLD EXCEEDED",yellow))
+	elif proc[0] > 150:
+		print("Es laufen momentan ["+colored(str(proc[0]),red)+"] Prozesse."+colored("                                                  WARNING: THRESHOLD EXCEEDED",red))
+	print("=======================================================================================================================")
+	print("D.h. ein Prozess verbraucht durchschnittlich ["+str(proc[1])+"] MB")
+	print("-----------------------------------------------------")
+	print("Die drei Ressource intensivsten Prozesse sind:")
+	print("1. ->",proc[2])
+	print("2. ->",proc[3])
+	print("3. ->",proc[4])
+	print("=======================================================================================================================")
+	#####################################################################################
+	print("")
+	print("Alle Internen Festplatten")
+	print("=======================================================================================================================")
+	for item in space:
+		print("| Driveletter: [{0: <2}] | Size: [{1:5.0f}] GB | Free: [{2:5.0f}] GB |".format(item[0][1],item[2][1],item[1][1]))
+	print("=======================================================================================================================")
+	#####################################################################################
 	
-#os.system("PAUSE>NUL")
+if __name__ == "__main__":
+
+	os.system("mode 120,60")
+	
+	while True:
+	
+		ram   = getRAM()
+		proc  = getProc()
+		space = getSpace()
+		visualize(ram,proc,space)
+		time.sleep(3)
